e412d6baffadeb5481d478117baf7b025e7ffb44
Pratham <62070812+prathami1@users.noreply.github.com>
2021-02-24 18:50:18 -0500

    Initial Commit
    Initial Commit of uploading files - introducing source code to Vercel for hosting

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import './App.css'
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: '64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <center>
+        <Header 
+            title = "News With Alan"
+            pretag = "A website with the latest news on tech and politics powered by "
+            posttag = "Alan"
+            hrefport = 'https://prathami1.studio'
+            prebutton = 'Built By'
+            name = "Pratham Inamdar"
+          />
+      </center>
+      <p></p>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <footer>
+        <center>
+        <Footer
+          href="https://alan.app/"
+          source="Alan AI"
+        />
+        </center>
+        </footer>
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/NewsCards/styles.js
@@ -0,0 +1,21 @@
+import { makeStyles, rgbToHex } from '@material-ui/core/styles';
+
+export default makeStyles({
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: '45vh',
+    padding: '10%',
+    borderRadius: 10,
+    color: 'black',
+  },
+  infoCard: {
+    display: 'flex', flexDirection: 'column', textAlign: 'center',
+  },
+  container: {
+    padding: '0 5%', width: '100%', margin: 0,
+  },
+});
